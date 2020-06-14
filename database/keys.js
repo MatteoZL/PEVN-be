499447e0f5ccca68f7df1875853b05d169ddcb30
MatteoZL <53958320+MatteoZL@users.noreply.github.com>
2020-06-13 23:49:47 -0500

    Heroku

--- database/keys.js
+++ database/keys.js
@@ -1,10 +1,16 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-    host: 'localhost',
+    /* host: 'localhost',
     user: 'postgres',
     password: 'password',
     database: 'pevn',
+    port: '5432' */
+
+    host: 'ec2-34-198-243-120.compute-1.amazonaws.com',
+    user: 'srwhsmjbsxdjvl',
+    password: 'add9d409205bf0e0aeff054c19baad682a7bdd0386c6a2f23c41498a8035e151',
+    database: 'd2huikpn51plh6',
     port: '5432'
 });
 
